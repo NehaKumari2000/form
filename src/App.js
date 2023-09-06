dde927c8c026fc1a371901913a0093d64a21b15a
neha kumari <nehamishra8210@gmail.com>
2023-09-06 18:58:58 +0530

    React Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label='I agree to the Terms and Conditions' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+          </div>
   );
 }
 
